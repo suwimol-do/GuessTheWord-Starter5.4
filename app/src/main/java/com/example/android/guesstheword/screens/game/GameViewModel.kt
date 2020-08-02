d35ebb6b1a59936967814ef87f3f0309529964e6
Suwimol <Suwimol.do@rmuti.ac.th>
2020-08-02 19:25:33 +0700

    5. Task: Add transformation for the LiveData

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -8,6 +9,22 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
+    private val timer: CountDownTimer
+
+
+    companion object {
+
+        // Time when the game is over
+        private const val DONE = 0L
+
+        // Countdown time interval
+        private const val ONE_SECOND = 1000L
+
+        // Total time for the game
+        private const val COUNTDOWN_TIME = 60000L
+
+    }
+
     // The current _word
     private val _word = MutableLiveData<String>()
 
@@ -15,6 +32,11 @@ class GameViewModel : ViewModel() {
     private val _score = MutableLiveData<Int>()
     val score: LiveData<Int>
         get() = _score
+
+    // Countdown time
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
 
     val word: LiveData<String>
         get() = _word
@@ -66,6 +88,23 @@ class GameViewModel : ViewModel() {
         Log.i("GameViewModel", "GameViewModel created!")
         resetList()
         nextWord()
+
+        // Creates a timer which triggers the end of the game when it finishes
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = millisUntilFinished/ONE_SECOND
+
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+
+            }
+        }
+
+        timer.start()
     }
 
     /**
@@ -74,6 +113,7 @@ class GameViewModel : ViewModel() {
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModel", "GameViewModel destroyed!")
+        timer.cancel()
     }
 
     /** Methods for updating the UI **/
@@ -86,15 +126,13 @@ class GameViewModel : ViewModel() {
         nextWord()
     }
 
-    /**
-     * Moves to the next _word in the list.
-     */
-    private fun nextWord() {
-        if (wordList.isEmpty()) {
-            onGameFinish()
 
+    private fun nextWord() {
+        // Shuffle the word list, if the list is empty
+        if (wordList.isEmpty()) {
+            resetList()
         } else {
-            //Select and remove a _word from the list
+            // Remove a word from the list
             _word.value = wordList.removeAt(0)
         }
     }
